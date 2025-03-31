04b99f6c0df9a3c5f0b3f89d3413efe410a2200c
Louise <emmalouisekarlsson@hotmail.com>
2025-03-31 10:28:08 +0200

    Secretword sorts list of words and sets secret word

--- GuessSecretWord/src/components/SecretWord.jsx
+++ GuessSecretWord/src/components/SecretWord.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
 
-function SecretWord() {
+export default function SecretWord({ onFilter, onDisplay }) {
   const [number, setNumber] = useState(4);
 
   function handleNumber(event) {
     setNumber(event.target.value);
   }
 
-  //Förhindra att kunna klicka om redan klickat
   const [repeat, setRep] = useState(true);
 
-  function handleRep() {
-    setRep(!repeat);
+  function handleRep(event) {
+    setRep(event.target.value);
   }
 
-  const [secret, setSecret] = useState("");
-
-  function handleSecret(word) {
-    setSecret(word);
+  const [error, setError] = useState("");
+  function handleError(err) {
+    setError(err);
   }
+
+  //Ska kunna öppnas åter om besökaren vill börja om från början med ett nytt ord
+  const [display, setDisplay] = useState(true);
+  function handleDisplay() {
+    setDisplay(!display);
+  }
+  console.log("display", display);
+
   //Ord ska hämtas via API Anrop
   const wordList = [
     "alla",
@@ -54,7 +60,8 @@ function SecretWord() {
     }
 
     if (sortedList.length == 0) {
-      console.log("No word");
+      handleError("No word that matches");
+      return;
     }
 
     if (remove.length > 0) {
@@ -65,42 +72,58 @@ function SecretWord() {
       } else {
         const randomWord =
           newLista[Math.floor(Math.random() * newLista.length)];
-        handleSecret(randomWord);
+        handleDisplay();
+        onDisplay(true);
+        onFilter(randomWord);
+        return;
       }
     } else {
       const randomWordNoRep =
         sortedList[Math.floor(Math.random() * sortedList.length)];
-      handleSecret(randomWordNoRep);
+      handleDisplay();
+      onDisplay(true);
+      onFilter(randomWordNoRep);
+      return;
     }
   }
 
   return (
     <>
-      <div className="flex flex-col">
-        <h2 className="text-3xl">Make your choice of word</h2>
-        <input
-          className="border-s-black bg-yellow-200"
-          type="number"
-          value={number}
-          onChange={handleNumber}
-          min={0}
-        />
-        <label>Choose length of word</label>
-        <p>Can the same letter occur more than once</p>
-        <input type="radio" defaultChecked name="repeat" onClick={handleRep} />
-        <label>Yes</label>
-        <input type="radio" name="repeat" onClick={handleRep} />
-        <label>No</label>
-        <button
-          onClick={() => {
-            sortWords(wordList, number, repeat);
-          }}
-        >
-          Choose secret word
-        </button>
-        <p>Hemligt ord: {secret}</p>
+      <div className="m-auto mt-6 max-w-[640px] border-black border-[0.5rem] rounded-lg">
+        <div className="p-4 flex flex-col items-center">
+          <h2 className="text-3xl mb-2">Make your choice of word</h2>
+          <label className="text-xl mb-2">Choose length of word</label>
+          <input
+            className="w-16 border-solid border-black border rounded-md font-bold p-1 text-center"
+            type="number"
+            value={number}
+            onChange={handleNumber}
+            min={0}
+          />
+
+          <p className="text-xl p-2">
+            Can the same letter occur more than once?
+          </p>
+          <select
+            value={repeat}
+            onChange={handleRep}
+            className="w-16 border-solid border-black border rounded-md font-bold p-1.5"
+          >
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <p>{error}</p>
+          <button
+            className="col-span-2 border rounded-md mt-4 bg-blue-600 text-white p-2"
+            onClick={() => {
+              handleError("");
+              sortWords(wordList, number, repeat);
+            }}
+          >
+            Choose secret word
+          </button>
+        </div>
       </div>
     </>
   );
 }
-export default SecretWord;
