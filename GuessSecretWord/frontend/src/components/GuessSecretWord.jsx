2b6c1d97745fa0e5f43be43a3d9952c70206e2ac
Louise <emmalouisekarlsson@hotmail.com>
2025-04-09 08:40:53 +0200

    Feedback about made guesses

--- GuessSecretWord/frontend/src/components/GuessSecretWord.jsx
+++ GuessSecretWord/frontend/src/components/GuessSecretWord.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import Button from "./button";
 
-function GuessSecretWord({ num, display }) {
-  console.log("GuessSecretword - display", display);
-
+function GuessSecretWord({ num, display, onCorrect }) {
   const [newGuess, setGuess] = useState("");
   function handleGuess(event) {
     setGuess(event.target.value);
@@ -13,7 +13,27 @@ function GuessSecretWord({ num, display }) {
     setError(err);
   }
 
-  function CheckAnswer(wordGuessed, num, count) {
+  const [result, setResult] = useState([]);
+
+  const [allResult, setAllResult] = useState([]);
+  function handleAllResult(result) {
+    setAllResult([...allResult, result]);
+  }
+
+  const [listGuesses, setListGuesses] = useState([]);
+  function handleListGuesses(list) {
+    setListGuesses(list);
+  }
+
+  useEffect(() => {
+    function reverseResult() {
+      const resultReversed = allResult.reverse();
+      handleListGuesses(resultReversed);
+    }
+    reverseResult(allResult);
+  }, [allResult]);
+
+  function CheckAnswer(wordGuessed, num) {
     if (wordGuessed === "") {
       handleError("Cant be empty");
       return;
@@ -23,23 +43,19 @@ function GuessSecretWord({ num, display }) {
       return;
     }
     setGuess("");
-    loadCheck(wordGuessed, count);
+    loadCheck(wordGuessed);
     return;
-  }
-
-  const [result, setResult] = useState([]);
-
-  const [allResult, setAllResult] = useState([]);
-  function handleAllResult(result) {
-    setAllResult([...allResult, result]);
   }
 
   async function loadCheck(guess) {
     const res = await fetch(`/game/guess/${guess}`);
     if (res.ok) {
       const result = await res.json();
-
-      setResult(result);
+      if (result === "correct") {
+        onCorrect(true);
+      } else {
+        setResult(result);
+      }
     } else {
       console.error("Error", res.status);
     }
@@ -57,65 +73,79 @@ function GuessSecretWord({ num, display }) {
 
   return (
     <>
-      <div className="m-auto max-w-[640px] border-black border-[0.5rem] rounded-lg">
-        <div className="p-4 flex flex-col items-center">
-          <h2 className="text-3xl mb-2">Guess the secret word</h2>
-          <label className="text-xl mb-2">Type your guess</label>
-          <input
-            className="border-solid border-black border rounded-md font-bold p-1"
-            type="text"
-            value={newGuess}
-            onChange={handleGuess}
-          />
-          <p>{error}</p>
-          <button
-            className="col-span-2 border rounded-md mt-4 bg-button-blue text-white pt-2 pb-2 pl-4 pr-4"
-            onClick={() => {
-              setError("");
-              handleAllResult(result);
-              CheckAnswer(newGuess, num);
-            }}
-          >
-            Make you guess
-          </button>
-        </div>
-        <div className="text-center">
-          <ul className="flex flex-row justify-center">
-            {result.map((word, index) => {
+      {display && (
+        <div className="m-auto mb-11 max-w-[640px] border-black border-[0.5rem] rounded-lg">
+          <div className="p-4 flex flex-col items-center">
+            <h2 className="text-3xl mb-2">Guess the secret word</h2>
+            <div className="mt-2 mb-b">
+              <p>
+                If your guess is'nt correct you will reseve feedback about each
+                letters occurrence in your guess, to use in coming guesses.
+              </p>
+              <div className=" mt-2 mb-2 flex flex-row justify-center">
+                <div className="h-10 w-10 mr-2 bg-[#579C05]"></div>
+                <p>Correct</p>
+                <div className="h-10 w-10 mr-2 ml-2 bg-[#FAB206]"></div>
+                <p>Misplaced</p>
+                <div className="h-10 w-10 mr-2 ml-2 bg-[#FE2D28]"></div>
+                <p>Incorrect</p>
+              </div>
+            </div>
+            <label className="text-xl mb-2 font-bold">Type your guess</label>
+            <input
+              className="border-solid border-black border rounded-md font-bold p-1"
+              type="text"
+              value={newGuess}
+              onChange={handleGuess}
+            />
+            <p className=" mt-2 text-red-600">{error}</p>
+            <Button
+              text={"Make you guess"}
+              onClick={() => {
+                setError("");
+                handleAllResult(result);
+                CheckAnswer(newGuess, num);
+              }}
+            ></Button>
+          </div>
+          <div className="text-center">
+            <ul className="flex flex-row justify-center">
+              {result.map((word, index) => {
+                return (
+                  <li
+                    key={index}
+                    className={`content-center h-16 w-16 m-1 border-black border-[0.5rem] rounded-md text-2xl font-bold ${getColor(
+                      word.secret
+                    )}`}
+                  >
+                    {word.letter}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="text-center">
+            {listGuesses.map((guess, index) => {
               return (
-                <li
-                  key={index}
-                  className={`content-center h-16 w-16 m-1 border-black border-[0.5rem] rounded-md text-2xl font-bold ${getColor(
-                    word.secret
-                  )}`}
-                >
-                  {word.letter}
-                </li>
+                <ul key={index} className="flex flex-row justify-center">
+                  {guess.map((word, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className={`content-center h-16 w-16 m-1 border-black border-[0.5rem] rounded-md text-2xl font-bold ${getColor(
+                          word.secret
+                        )}`}
+                      >
+                        {word.letter}
+                      </li>
+                    );
+                  })}
+                </ul>
               );
             })}
-          </ul>
+          </div>
         </div>
-        <div className="text-center">
-          {allResult.map((guess, index) => {
-            return (
-              <ul key={index} className="flex flex-row justify-center">
-                {guess.map((word, index) => {
-                  return (
-                    <li
-                      key={index}
-                      className={`content-center h-16 w-16 m-1 border-black border-[0.5rem] rounded-md text-2xl font-bold ${getColor(
-                        word.secret
-                      )}`}
-                    >
-                      {word.letter}
-                    </li>
-                  );
-                })}
-              </ul>
-            );
-          })}
-        </div>
-      </div>
+      )}
     </>
   );
 }
