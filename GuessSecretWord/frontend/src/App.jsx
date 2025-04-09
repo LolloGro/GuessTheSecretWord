615c550ca91207820a39127b252283acdd1ce2ed
Louise <emmalouisekarlsson@hotmail.com>
2025-04-09 08:42:21 +0200

    Handle if display

--- GuessSecretWord/frontend/src/App.jsx
+++ GuessSecretWord/frontend/src/App.jsx
@@ -10,17 +10,22 @@ function App() {
     setNum(num);
   }
 
-  const [display, setDisplay] = useState("hide");
+  const [display, setDisplay] = useState(false);
   function handleDisplay(show) {
     setDisplay(show);
+  }
+
+  const [register, setRegister] = useState(false);
+  function handleRegister(show) {
+    setRegister(show);
   }
 
   return (
     <>
       <HomePage />
       <SecretWord onNum={handleNum} onDisplay={handleDisplay} />
-      <GuessSecretWord num={num} display={display} />
-      <RegHighscore />
+      <GuessSecretWord num={num} display={display} onCorrect={handleRegister} />
+      <RegHighscore register={register} onDisplay={handleDisplay} />
     </>
   );
 }
