e20435438b80e73b0cb40175bad8a711f8dc2f71
Louise <emmalouisekarlsson@hotmail.com>
2025-04-09 21:04:38 +0200

    App components updated

--- GuessSecretWord/frontend/src/App.jsx
+++ GuessSecretWord/frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import HomePage from "./components/HomePage";
 import GuessSecretWord from "./components/GuessSecretWord";
 import SecretWord from "./components/SecretWord";
-import RegHighscore from "./components/RegHighscore";
 
 function App() {
   const [num, setNum] = useState(0);
@@ -15,17 +14,16 @@ function App() {
     setDisplay(show);
   }
 
-  const [register, setRegister] = useState(false);
-  function handleRegister(show) {
-    setRegister(show);
+  const [id, setId] = useState("");
+  function handleId(uniq) {
+    setId(uniq);
   }
 
   return (
     <>
       <HomePage />
-      <SecretWord onNum={handleNum} onDisplay={handleDisplay} />
-      <GuessSecretWord num={num} display={display} onCorrect={handleRegister} />
-      <RegHighscore register={register} onDisplay={handleDisplay} />
+      <SecretWord onNum={handleNum} onDisplay={handleDisplay} onID={handleId} />
+      <GuessSecretWord num={num} display={display} id={id} />
     </>
   );
 }
