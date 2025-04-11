b8af21174f18480ce518d9ecb60fbde0f48c363a
Louise <emmalouisekarlsson@hotmail.com>
2025-04-11 23:23:00 +0200

    Mongoose schema updated

--- GuessSecretWord/server/src/models.js
+++ GuessSecretWord/server/src/models.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
 const resultSchema = new mongoose.Schema({
+  playerID: String,
   name: String,
+  startTime: Number,
+  stopTime: Number,
   time: Number,
   count: Number,
+  gusses: Array,
   letter: Number,
   repeat: String,
 });
