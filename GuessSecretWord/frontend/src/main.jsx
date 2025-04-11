8501731e5f0f7da027380e961550ac3ad2fa4229
Louise <emmalouisekarlsson@hotmail.com>
2025-04-11 23:21:19 +0200

    Prevent load of react components when changing page

--- GuessSecretWord/frontend/src/main.jsx
+++ GuessSecretWord/frontend/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("findMe")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+if (document.getElementById("findMe")) {
+  createRoot(document.getElementById("findMe")).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
