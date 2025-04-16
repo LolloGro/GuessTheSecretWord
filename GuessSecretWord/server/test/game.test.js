9b9773f60822bc7ccbffbb323fbef09ae0276134
Louise <emmalouisekarlsson@hotmail.com>
2025-04-16 21:42:45 +0200

    Removed unused images

--- GuessSecretWord/server/test/game.test.js
+++
@@ -1,3 +0,0 @@
-import { describe, it, expect } from "@jest/globals";
-import loadPage from "../src/app.js";
-import supertest from "supertest";
--- GuessSecretWord/server/typescript/checkGuess.ts
+++ GuessSecretWord/server/typescript/checkGuess.ts
@@ -19,25 +19,17 @@ export default function checkGuess(
   const secret = answer.split("");
   const guessedWord = guess.split("");
 
-  const checkSecret = [];
+  const checkSecret: { secret: string; letter: string }[] = [];
 
   for (let i = 0; i < secret.length; i++) {
     if (secret[i] == guessedWord[i]) {
       checkSecret.push({ secret: "Correct", letter: guessedWord[i] });
     } else if (secret.includes(guessedWord[i])) {
-      const kolla = guessedWord.filter((b) => b == guessedWord[i]);
-      const referens = secret.filter((a) => a == guessedWord[i]);
-      if (kolla.length <= referens.length) {
-        checkSecret.push({ secret: "Misplaced", letter: guessedWord[i] });
-      } else {
-        checkSecret.push({ secret: "Incorrect", letter: guessedWord[i] });
-      }
+      checkSecret.push({ secret: "Misplaced", letter: guessedWord[i] });
     } else {
       checkSecret.push({ secret: "Incorrect", letter: guessedWord[i] });
     }
   }
-
-  //Bugg: om skriver en gissning med två lika bokstäver, men det bara finns sådan i det hemliga ordet. Anges att bägge bokstäver är incorrect
 
   const feedback = { checkSecret, correct, stopTime, totalTime };
 
